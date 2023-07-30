2a03e0b764c5c5d0f2c805c9365d564f61e1eaed
michael <michael@meinerseitz.de>
2023-07-30 13:40:24 +0200

    list is now visible, but only one item

---
+++ main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.loadFile('index.html');
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+ipcMain.on('get-dropped-files', (event, files) => {
+  event.returnValue = files;
+});
